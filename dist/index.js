2f0ef4aa57219e2163eb9b10272b3d9eecf42e27
Prabin Nepal <prabinnepal1996@gmail.com>
2023-05-13 01:35:35 -0500

    Graphql and apollo server setup

--- dist/index.js
+++ dist/index.js
@@ -4,13 +4,26 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@mikro-orm/core");
-const Post_1 = require("./entities/Post");
 const mikro_orm_config_1 = __importDefault(require("./mikro-orm.config"));
+const express_1 = __importDefault(require("express"));
+const apollo_server_express_1 = require("apollo-server-express");
+const type_graphql_1 = require("type-graphql");
+const hello_1 = require("./resolvers/hello");
 const main = async () => {
     const orm = await core_1.MikroORM.init(mikro_orm_config_1.default);
-    const emFork = orm.em.fork();
-    const posts = await emFork.find(Post_1.Post, {});
-    console.log(posts);
+    await orm.getMigrator().up();
+    const app = (0, express_1.default)();
+    const apolloServer = new apollo_server_express_1.ApolloServer({
+        schema: await (0, type_graphql_1.buildSchema)({
+            resolvers: [hello_1.HelloResolver],
+            validate: false
+        }),
+    });
+    await apolloServer.start();
+    apolloServer.applyMiddleware({ app });
+    app.listen(4000, () => {
+        console.log("server starting at http://localhost:4000");
+    });
 };
 main();
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,33 @@ import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants"; 
 import { Post } from "./entities/Post";
 import mikroConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { HelloResolver } from "./resolvers/hello";
 
 const main = async () => {
     const orm = await MikroORM.init(mikroConfig); 
-    const emFork = orm.em.fork();
-    // await  orm.getMigrator().up();
-    // const post = emFork.create(Post, { title: "Hello" });
-    // await emFork.persistAndFlush(post);
-    const posts = await emFork.find(Post, {});
-    console.log(posts)
+    await orm.getMigrator().up();
+    // const emFork = orm.em.fork();
+
+    const app = express()
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver],
+            validate: false
+        }),
+    });
+    await apolloServer.start();
+    apolloServer.applyMiddleware({ app }); 
+
+    app.listen(4000, () => {
+        console.log("server starting at http://localhost:4000");
+    });
+
+
+
 
 
 }
